10d87a6bdd0f4795c9934c63bdf1e6c3466511c4
rawraw <995106979@qq.com>
2017-09-04 14:45:58 +0800

    训练档案

---
+++ application/admin/controller/Plan.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File: Plan.php
+ * User: Administrator
+ * Date: 2017-09-04 14:24
+ */
+namespace app\admin\controller;
+/**
+ * Class Plan
+ * @package 训练档案控制器
+ */
+class Plan extends Admin {
+	/**
+	 * 档案列表
+	 */
+	public function index(){
+
+	}
+}
