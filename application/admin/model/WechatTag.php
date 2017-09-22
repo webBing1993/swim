fab00ea35e89f69f9e6b58731426f8f6b30a799a
ben <750405122@qq.com>
2017-09-22 11:14:50 +0800

    同步

--- application/admin/model/WechatTag.php
+++ application/admin/model/WechatTag.php
@@ -17,12 +17,14 @@ class WechatTag extends Base {
     const TAG_ASSISTANT = 3;
     const TAG_STUDENT = 4;
     const TAG_VISITOR = 5;
+    const TAG_STUDENT_READY = 6;
     const TAG_TO_METYPE = [
         self::TAG_LEADER  => WechatUser::MEMBER_TYPE_NULL,//领导
         self::TAG_HEAD_COACH  => WechatUser::MEMBER_TYPE_COACH,//主教练
         self::TAG_ASSISTANT  => WechatUser::MEMBER_TYPE_COACH,//助教
         self::TAG_STUDENT  => WechatUser::MEMBER_TYPE_STUDENT,//学员
         self::TAG_VISITOR  => WechatUser::MEMBER_TYPE_NULL,//游客
+        self::TAG_STUDENT_READY  => WechatUser::MEMBER_TYPE_STUDENT,//预备学员
     ];
     public function Wechat_user() {
         return $this->belongsToMany('WechatUser','WechatTag');
