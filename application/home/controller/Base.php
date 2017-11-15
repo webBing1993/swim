54f28624ffa30fe0fb985cb1c5dd73503dc07898
1324664410@qq.com <hui00544>
2017-11-15 11:05:21 +0800

    Merge branch 'master' of E:\party\swim with conflicts.

--- application/home/controller/Base.php
+++ application/home/controller/Base.php
@@ -25,9 +25,7 @@ class Base extends Controller
 {
     public function _initialize()
     {
-        session('userId', 'xsfyyjxuxing439126');//领导 徐星
-//        session('userId', '13957110303');//主教练 陈佳
-//        session('userId', '13777483530');//助教 蔡晓燕
+        session('userId', '13588109527');
         session('requestUri', 'http://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
         $userId = session('userId');
 //        if($userId=='ben'){
